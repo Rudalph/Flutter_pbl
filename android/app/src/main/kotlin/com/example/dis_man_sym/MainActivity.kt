8f8ffcb222ed2b3868457800226397e9542414c7
rudalph <gonsalvesrudalph@gmail.com>
2023-01-30 23:14:23 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/dis_man_sym/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dis_man_sym
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp
+      (
+         title: "Disaster Management System",
+         home: Scaffold
+           (
+           backgroundColor: Colors.white,
+           appBar: AppBar
+             (
+             title: Text("Dis-Man-Sym",style: TextStyle(color: Colors.black),),
+             backgroundColor: Colors.white,
+             leading: Icon(Icons.home, color: Colors.black,),
+           ),
+
+           body: Row(
+             children:
+             [
+               Container
+                 (
+                 margin: EdgeInsets.only(top:100, left: 75),
+                 height:100,
+                 width: 100,
+                 color: Colors.grey,
+                 child: TextButton
+                   (
+                   onPressed: () {},
+                   child: Text("Disasters", style: TextStyle(color: Colors.black),),
+                 ),
+
+               ),
+
+               Container
+                 (
+                 margin: EdgeInsets.only(top:100, left: 100),
+                 height:100,
+                 width: 100,
+                 color: Colors.grey,
+                 child: TextButton
+                   (
+                   onPressed: () {},
+                   child: Text("Do's & Dont's", style: TextStyle(color: Colors.black),),
+                 ),
+
+               ),
+
+             ],
+           )
+         ),
+    );
+  }
+}

29d978230f65e30fd6b59eacdde5118ac62dc392
Alroy <alroypereira05@gmail.com>
2023-01-31 19:01:05 +0530

    changed the color of buttom and appBar to purple

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(const MyApp());
 }
 
@@ -10,53 +9,51 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp
-      (
-         title: "Disaster Management System",
-         home: Scaffold
-           (
-           backgroundColor: Colors.white,
-           appBar: AppBar
-             (
-             title: Text("Dis-Man-Sym",style: TextStyle(color: Colors.black),),
-             backgroundColor: Colors.white,
-             leading: Icon(Icons.home, color: Colors.black,),
-           ),
-
-           body: Row(
-             children:
-             [
-               Container
-                 (
-                 margin: EdgeInsets.only(top:100, left: 75),
-                 height:100,
-                 width: 100,
-                 color: Colors.grey,
-                 child: TextButton
-                   (
-                   onPressed: () {},
-                   child: Text("Disasters", style: TextStyle(color: Colors.black),),
-                 ),
-
-               ),
-
-               Container
-                 (
-                 margin: EdgeInsets.only(top:100, left: 100),
-                 height:100,
-                 width: 100,
-                 color: Colors.grey,
-                 child: TextButton
-                   (
-                   onPressed: () {},
-                   child: Text("Do's & Dont's", style: TextStyle(color: Colors.black),),
-                 ),
-
-               ),
-
-             ],
-           )
-         ),
+    return MaterialApp(
+      title: "Disaster Management System",
+      home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text(
+              "Dis-Man-Sym",
+              style: TextStyle(color: Colors.black),
+            ),
+            backgroundColor: Colors.purple,
+            leading: Icon(
+              Icons.home,
+              color: Colors.black,
+            ),
+          ),
+          body: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 100, left: 75),
+                height: 100,
+                width: 100,
+                color: Colors.purple,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Disasters",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 100, left: 100),
+                height: 100,
+                width: 100,
+                color: Colors.purple,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Do's & Dont's",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }

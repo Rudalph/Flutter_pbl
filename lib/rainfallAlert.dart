3888b326b34962083c3c4a2dca2f4e376567906b
Alroy <alroypereira05@gmail.com>
2023-02-04 14:24:08 +0530

    add weather info and improved the UI

--- lib/rainfallAlert.dart
+++ lib/rainfallAlert.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class WeatherScreen extends StatefulWidget {
   @override
@@ -19,9 +21,8 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   Future<void> getWeatherData() async {
     String url =
-        'http://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=$apiKey&units=metric';
+        'http://api.openweathermap.org/data/2.5/weather?q=London&appid=$apiKey&units=metric';
     Uri uri = Uri.parse(url);
-// Now you can pass the `uri` object to a function or method that expects a `Uri`.
 
     var response = await http.get(uri);
     if (response.statusCode == 200) {
@@ -41,11 +42,88 @@ class _WeatherScreenState extends State<WeatherScreen> {
               child: CircularProgressIndicator(),
             )
           : Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.blue,
+                    Colors.blue,
+                  ],
+                ),
+              ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('Temperature: ${weatherData['main']['temp']}°C'),
-                  Text('City: ${weatherData['name']}'),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white70,
+                    ),
+                    child: Icon(
+                      FontAwesomeIcons.cloudSun,
+                      size: 100,
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Temperature: ${weatherData['main']['temp']}°C',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'City: ${weatherData['name']}',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Weather: ${weatherData['weather'][0]['description']}',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        getWeatherData();
+                      },
+                      child: Text(
+                        'Refresh',
+                        style: TextStyle(
+                          color: Colors.blue[900],
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),

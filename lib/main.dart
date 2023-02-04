1617ac80b0d50723e2c1bf97debd8ed0680e3cca
Alroy <alroypereira05@gmail.com>
2023-02-04 16:21:33 +0530

    Extras

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:dis_man_sym/DisastersScreen.dart';
 import 'package:dis_man_sym/rainfallAlert.dart';
 import 'package:flutter/material.dart';

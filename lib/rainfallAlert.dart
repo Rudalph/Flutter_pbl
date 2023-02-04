fe4e098119b1f58d3a294c10a623e69546de7560
Alroy <alroypereira05@gmail.com>
2023-02-04 11:32:15 +0530

    Rainfall api and button added

---
+++ lib/rainfallAlert.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherScreen extends StatefulWidget {
+  @override
+  _WeatherScreenState createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  String apiKey = '54a4215d65eb60a2a0c49c14c289a227';
+  dynamic weatherData;
+
+  @override
+  void initState() {
+    super.initState();
+    getWeatherData();
+  }
+
+  Future<void> getWeatherData() async {
+    var response = await http.get(
+        'http://api.openweathermap.org/data/2.5/weather?q=London&appid=$apiKey');
+    if (response.statusCode == 200) {
+      setState(() {
+        weatherData = json.decode(response.body);
+      });
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: weatherData == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Temperature: ${weatherData['main']['temp']}°C'),
+                  Text('City: ${weatherData['name']}'),
+                ],
+              ),
+            ),
+    );
+  }
+}

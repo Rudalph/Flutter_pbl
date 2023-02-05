0d29d2e027ccf1a658033a9cfdc2a2002c47236d
Rudalph <gonsalvesrudalph@gmail.com>
2023-02-05 19:58:47 +0530

    alerts page updated

--- lib/alerts.dart
+++ lib/alerts.dart
@@ -7,6 +7,7 @@ class alerts extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("ALERTS",style: TextStyle(color: Colors.black),),
         backgroundColor: Colors.purple,
         leading: IconButton
         (
@@ -17,6 +18,89 @@ class alerts extends StatelessWidget {
         ),
 
       ),
+     body:Column(
+       children: [
+         Container
+         (
+             child: Padding
+          (
+            padding: EdgeInsets.all(28.0),
+            child: Center
+            (
+              child: Container
+              (
+                height: 200,
+                width: 300,
+                child: Card
+                (
+                  color: Colors.purple,
+                  shape: RoundedRectangleBorder
+                  (
+                    borderRadius: BorderRadius.circular(30)
+                  ),
+                  elevation: 100,
+
+                  child: Column
+                    (
+                    mainAxisSize: MainAxisSize.min,
+                    children:
+                    [
+                      ListTile
+                        (
+                        leading: Icon(Icons.web_asset_outlined),
+                        title: Text("NOTICE"),
+                        subtitle: Text("ALERTS GENERATED HERE ARE BASED ON THE DATA THAT IS DISPLAYED ON WEBSITE....DATA SCRAPING TECHNIQUE AND PREVIOUS YEARS DATA IS USED TO ANALYSE AND GENERATE ALERTS"),
+                      ),
+
+                      ButtonBar
+                        (
+                        children:
+                        [
+
+                          ElevatedButton
+                            (
+                              onPressed: () {},
+                              child: Text("HELPLINE"),
+                              style: ElevatedButton.styleFrom
+                              (
+                                primary: Color.fromARGB(255, 43, 3, 50)
+                              ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ),
+              ),
+            ),
+          ),
+         ),
+         Container
+         (
+          child: Padding
+          (
+              padding: EdgeInsets.all(28.0),
+            child: Center
+            (
+              child: Container
+              (
+                height: 200,
+                width: 300,
+                child: Card
+                (
+                  color: Colors.purple,
+                  shape: RoundedRectangleBorder
+                  (
+                    borderRadius: BorderRadius.circular(30)
+                  ),
+                  elevation: 100,
+                ),
+              ),
+            ),  
+          ),
+         )
+       ],
+     ) 
     );
   }
 }

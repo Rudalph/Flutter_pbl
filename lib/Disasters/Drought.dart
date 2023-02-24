596f0303cc8d02520dda4ad0e26541e4b65e4a4d
NathanDias11 <nathandias177@gmail.com>
2023-02-24 14:14:31 +0530

    changes in drought

--- lib/Disasters/Drought.dart
+++ lib/Disasters/Drought.dart
@@ -18,7 +18,7 @@ class DisasterDroughtScreen extends StatelessWidget {
                 return Container(
                   padding: EdgeInsets.all(25),
                   child: Text(
-                    'Drought is defined as either the absence or deficit of rainfall relative to the region\'s usual pattern over a protracted period of time, which causes overall hardship in the community. The demand that people have for the natural water supply and the natural occurrence that supplies the water in a particular geographic area interact. All climatic regimes experience drought, which is a regular occurrence that is often classified by its extent in space, intensity, and length.',
+                    'Drought is defined as either the absence or deficit of rainfall relative to the region\'s usual pattern over a protracted period of time, which causes overall hardship in the community. The demand that people have for the natural water supply and the natural occurrence that supplies the water in a particular geographic area interact. All climatic regimes experience drought, which is a regular occurrence that is often classified by its extent in space, intensity, and length.Mechanisms of producing precipitation include convective, stratiform, and orographic rainfall. Convective processes involve strong vertical motions that can cause the overturning of the atmosphere in that location within an hour and cause heavy precipitation, while stratiform processes involve weaker upward motions and less intense precipitation over a longer duration. Precipitation can be divided into three categories, based on whether it falls as liquid water, liquid water that freezes on contact with the surface, or ice. Droughts occur mainly in areas where normal levels of rainfall are, in themselves, low.',
                     style: TextStyle(fontSize: 25),
                   ),
                 );
@@ -35,7 +35,7 @@ class DisasterDroughtScreen extends StatelessWidget {
           ),
           SizedBox(height: 10),
           Text(
-            'Drought is defined as either the absence or deficit of rainfall relative to the region\'s usual pattern over a protracted period of time, which causes overall hardship in the community. The demand that people have for the natural water supply and the natural occurrence that supplies the water in a particular geographic area interact. All climatic regimes experience drought, which is a regular occurrence that is often classified by its extent in space, intensity, and length.',
+            'Drought is a prolonged dry period in the natural climate cycle that can occur anywhere in the world. It is a slow-onset disaster characterized by the lack of precipitation, resulting in a water shortage. Drought can have a serious impact on health, agriculture, economies, energy and the environment.',
             style: TextStyle(
               fontSize: 20,
             ),

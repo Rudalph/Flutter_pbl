45f08a8945016492de40f23ed36aca2ae72bc919
Alroy <alroypereira05@gmail.com>
2023-02-24 12:45:19 +0530

    fixed

--- lib/Disasters/Drought.dart
+++ lib/Disasters/Drought.dart
@@ -21,26 +21,26 @@ class DisasterDroughtScreen extends StatelessWidget {
                     'Drought is defined as either the absence or deficit of rainfall relative to the region\'s usual pattern over a protracted period of time, which causes overall hardship in the community. The demand that people have for the natural water supply and the natural occurrence that supplies the water in a particular geographic area interact. All climatic regimes experience drought, which is a regular occurrence that is often classified by its extent in space, intensity, and length.',
                     style: TextStyle(fontSize: 25),
                   ),
-                ],
-              ),
-              SizedBox(height: 25),
-              Text(
-                'Definition:',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Drought is defined as either the absence or deficit of rainfall relative to the region\'s usual pattern over a protracted period of time, which causes overall hardship in the community. The demand that people have for the natural water supply and the natural occurrence that supplies the water in a particular geographic area interact. All climatic regimes experience drought, which is a regular occurrence that is often classified by its extent in space, intensity, and length.',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ],
+                );
+              },
+            ),
           ),
-        ),
+          SizedBox(height: 25),
+          Text(
+            'Definition:',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Drought is defined as either the absence or deficit of rainfall relative to the region\'s usual pattern over a protracted period of time, which causes overall hardship in the community. The demand that people have for the natural water supply and the natural occurrence that supplies the water in a particular geographic area interact. All climatic regimes experience drought, which is a regular occurrence that is often classified by its extent in space, intensity, and length.',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ],
       ),
     );
   }

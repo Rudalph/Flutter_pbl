3fad344271d75623bdba19ae9679078bb5d3ecd1
Alroy <alroypereira05@gmail.com>
2023-04-01 17:31:31 +0530

    Errors fixed

--- lib/Disasters/floods.dart
+++ lib/Disasters/floods.dart
@@ -6,6 +6,7 @@ class DisasterFloodScreen extends StatefulWidget {
 }
 
 class _DisasterFloodScreenState extends State<DisasterFloodScreen> {
+  String headText = "FLOOD";
   final translator = GoogleTranslator();
   String _translatedText = "";
   String _selectedLanguage = "en"; // Default language is English
@@ -41,11 +42,8 @@ class _DisasterFloodScreenState extends State<DisasterFloodScreen> {
 
   String getTextToTranslate() {
     String text = "";
-    text = 'Flood';
-    text = 'INFO:';
-    text = "India";
-    text = 'EMERGENCY KIT:';
-    text = '- Battery';
+    text = headText +
+        '\n\nA low-pressure area characterized by rapid and frequently damaging air circulation is what generates cyclones. Storms and unfavorable weather are frequently present during cyclones. In the Northern hemisphere, the air moves anticlockwise, whereas in the Southern hemisphere, it moves clockwise. Extra tropical cyclones, sometimes known as temperate cyclones, and tropical cyclones are the two categories under which cyclones fall. The name "cyclone" comes from the Greek word "cyclos," which means a snake\'s coils. Henry Peddington came up with the term because tropical storms in the Arabian Sea and Bay of Bengal resemble coiled sea serpents.\n\nThe development cycle of tropical cyclones may be divided into three stages:\n\nFormation and Initial Development Stage The formation and initial development of a cyclonic storm depends upon various conditions. These are:\n\nA warm sea (a temperature in excess of 26 degrees Celsius to a depth of 60 m) with abundant and turbulent transfer of water vapor to the overlying atmosphere by evaporation.\nAtmospheric instability encouraging formation of massive vertical cumulus clouds due to convection with condensation of rising air above ocean surface.\n\nEMERGENCY KIT:\n- Battery operated torch\n- Extra batteries\n- Battery operated radio\n- First aid kit and essential medicines\n- Important papers (Ration card, Voter ID card, Aadhar card etc)\n- Emergency food (dry items) and water (packed and sealed)\n- Candles and matches in a waterproof container\n- Knife\n- Chlorine tablets or powdered water purifiers\n- Cash, Aadhar Card, and Ration Card\n- Thick ropes and cords\n- Shoes';
     return text;
   }
 
@@ -93,66 +91,11 @@ class _DisasterFloodScreenState extends State<DisasterFloodScreen> {
                       ? getTextToTranslate()
                       : _translatedText,
                   style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
                     fontFamily: 'Raleway',
                     color: Colors.white,
                   ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      _translatedText.isEmpty
-                          ? getTextToTranslate()
-                          : _translatedText,
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'Raleway',
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      _translatedText.isEmpty
-                          ? getTextToTranslate()
-                          : _translatedText,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.w300,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      _translatedText.isEmpty
-                          ? getTextToTranslate()
-                          : _translatedText,
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'Raleway',
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      _translatedText.isEmpty
-                          ? getTextToTranslate()
-                          : _translatedText,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'Raleway',
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
                 ),
               ),
             ],

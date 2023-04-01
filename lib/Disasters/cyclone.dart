e8f3b6673c07da5c082528cec702a304b2585fa5
Alroy <alroypereira05@gmail.com>
2023-04-01 19:55:40 +0530

    cycloneS

--- lib/Disasters/cyclone.dart
+++ lib/Disasters/cyclone.dart
@@ -1,17 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
 
-class DisasterCycloneScreen extends StatelessWidget {
+class DisasterCycloneScreen extends StatefulWidget {
+  _DisasterCycloneScreenState createState() => _DisasterCycloneScreenState();
+}
+
+class _DisasterCycloneScreenState extends State<DisasterCycloneScreen> {
+  String headText = "CYCLONE";
+  final translator = GoogleTranslator();
+  String _translatedText = "";
+  String _selectedLanguage = "en"; // Default language is English
+
+  List<PopupMenuEntry<String>> _buildLanguageMenuItems() {
+    return [
+      PopupMenuItem<String>(
+        value: "en",
+        child: Text("English"),
+      ),
+      PopupMenuItem<String>(
+        value: "hi",
+        child: Text("Hindi"),
+      ),
+      PopupMenuItem<String>(
+        value: "mr",
+        child: Text("Marathi"),
+      ),
+    ];
+  }
+
+  Future<void> _translateText() async {
+    String textToTranslate = "";
+    setState(() {
+      textToTranslate = getTextToTranslate();
+    });
+    Translation translation =
+        await translator.translate(textToTranslate, to: _selectedLanguage);
+    setState(() {
+      _translatedText = translation.text;
+    });
+  }
+
+  String getTextToTranslate() {
+    String text = "";
+    text = headText +
+        '\n\nA low-pressure area characterized by rapid and frequently damaging air circulation is what generates cyclones. Storms and unfavorable weather are frequently present during cyclones. In the Northern hemisphere, the air moves anticlockwise, whereas in the Southern hemisphere, it moves clockwise. Extra tropical cyclones, sometimes known as temperate cyclones, and tropical cyclones are the two categories under which cyclones fall. The name "cyclone" comes from the Greek word "cyclos," which means a  coils. Henry Peddington came up with the term because tropical storms in the Arabian Sea and Bay of Bengal resemble coiled sea serpents.\n\nThe development cycle of tropical cyclones may be divided into three stages:\n\nFormation and Initial Development Stage The formation and initial development of a cyclonic storm depends upon various conditions. These are:\n\nA warm sea (a temperature in excess of 26 degrees Celsius to a depth of 60 m) with abundant and turbulent transfer of water vapor to the overlying atmosphere by evaporation.\nAtmospheric instability encouraging formation of massive vertical cumulus clouds due to convection with condensation of rising air above ocean surface.';
+    return text;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Cyclones",
+          "CYCLONE",
           style: TextStyle(fontFamily: 'Raleway'),
         ),
         backgroundColor: Color.fromARGB(255, 236, 171, 118),
         foregroundColor: Colors.white,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            icon: Icon(Icons.translate),
+            onSelected: (language) {
+              setState(() {
+                _selectedLanguage = language;
+              });
+              _translateText(); // Call _translateText() to update the translated text
+            },
+            itemBuilder: (BuildContext context) {
+              return _buildLanguageMenuItems();
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Container(
@@ -27,60 +87,15 @@ class DisasterCycloneScreen extends StatelessWidget {
               Container(
                 padding: EdgeInsets.all(20),
                 child: Text(
-                  'CYCLONES',
+                  _translatedText.isEmpty
+                      ? getTextToTranslate()
+                      : _translatedText,
                   style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
                     fontFamily: 'Raleway',
                     color: Colors.white,
                   ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'INFO:',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'Raleway',
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'A low-pressure area characterized by rapid and frequently damaging air circulation is what generates cyclones. Storms and unfavorable weather are frequently present during cyclones. In the Northern hemisphere, the air moves anticlockwise, whereas in the Southern hemisphere, it moves clockwise. Extra tropical cyclones, sometimes known as temperate cyclones, and tropical cyclones are the two categories under which cyclones fall. The name "cyclone" comes from the Greek word "cyclos," which means a snake\'s coils. Henry Peddington came up with the term because tropical storms in the Arabian Sea and Bay of Bengal resemble coiled sea serpents.\n\nThe development cycle of tropical cyclones may be divided into three stages:\n\nFormation and Initial Development Stage The formation and initial development of a cyclonic storm depends upon various conditions. These are:\n\nA warm sea (a temperature in excess of 26 degrees Celsius to a depth of 60 m) with abundant and turbulent transfer of water vapor to the overlying atmosphere by evaporation.\nAtmospheric instability encouraging formation of massive vertical cumulus clouds due to convection with condensation of rising air above ocean surface.',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.w300,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'EMERGENCY KIT:',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'Raleway',
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      '- Battery operated torch\n- Extra batteries\n- Battery operated radio\n- First aid kit and essential medicines\n- Important papers (Ration card, Voter ID card, Aadhar card etc)\n- Emergency food (dry items) and water (packed and sealed)\n- Candles and matches in a waterproof container\n- Knife\n- Chlorine tablets or powdered water purifiers\n- Cash, Aadhar Card, and Ration Card\n- Thick ropes and cords\n- Shoes',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'Raleway',
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
                 ),
               ),
             ],
